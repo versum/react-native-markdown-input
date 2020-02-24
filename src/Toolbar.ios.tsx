e6c685f1dba75ef2b54fe4e0148aa9816f847dcd
Michal Kantor <kantorm96@gmail.com>
2020-02-24 14:36:24 +0100

    test: toolbar render tests

---
+++ src/Toolbar.ios.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { InputAccessoryView, ScrollView } from 'react-native';
+
+import { ToolbarProps } from './componentTypes';
+import ToolbarItem from './ToolbarItem';
+
+const CONTROLS = [
+  'bold',
+  'italic',
+  'heading',
+  'link',
+  'orderedList',
+  'unorderedList',
+];
+
+const Toolbar = ({ nativeID }: ToolbarProps) => {
+  return (
+    <InputAccessoryView nativeID={nativeID}>
+      <ScrollView horizontal keyboardShouldPersistTaps="always">
+        {CONTROLS.map(item => (
+          <ToolbarItem controlName={item} key={item} testID={`${item}Item`} />
+        ))}
+      </ScrollView>
+    </InputAccessoryView>
+  );
+};
+
+export default Toolbar;
--- src/__tests__/MarkdownInput.test.tsx
+++ src/__tests__/MarkdownInput.test.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
-import { render } from 'react-native-testing-library';
+import {
+  render,
+  fireEvent,
+  act,
+  waitForElement,
+} from 'react-native-testing-library';
 
 import MarkdownInput from '../MarkdownInput';
 
 describe('MarkdownInput', () => {
   test('should render multiline input', () => {
-    const { getByTestId } = render(<MarkdownInput />);
+    const { getByTestId } = render(
+      <MarkdownInput inputAccessoryViewID="testInput" />
+    );
 
     expect(getByTestId('markdownInput')).not.toBeNull();
     expect(getByTestId('markdownInput').props.multiline).toBe(true);
   });
 
-  test.todo('should render toolbar when keyboard is visible');
+  test('should render toolbar when keyboard is visible', async () => {
+    const { getByTestId, queryByTestId } = render(
+      <MarkdownInput inputAccessoryViewID="testInput" />
+    );
+
+    await act(async () => {
+      fireEvent(getByTestId('markdownInput'), 'focus');
+      await waitForElement(() => getByTestId('toolbar'));
+    });
+
+    expect(queryByTestId('toolbar')).not.toBeNull();
+  });
+
+  test('should render controls for markdown syntax', async () => {
+    const { getByTestId, queryByTestId } = render(
+      <MarkdownInput inputAccessoryViewID="testInput" />
+    );
+
+    await act(async () => {
+      fireEvent(getByTestId('markdownInput'), 'focus');
+      await waitForElement(() => getByTestId('toolbar'));
+    });
+
+    const visibleControls = [
+      'bold',
+      'italic',
+      'link',
+      'heading',
+      'orderedList',
+      'unorderedList',
+    ];
+
+    visibleControls.forEach(control => {
+      expect(queryByTestId(`${control}Item`)).not.toBeNull();
+    });
+  });
 });
