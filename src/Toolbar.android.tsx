576ad60327f3c5ec2e2940e088f8fbe9b2eafde6
Michal Kantor <kantorm96@gmail.com>
2020-02-24 15:02:20 +0100

    test: adapt android toolbar to render tests

--- src/Toolbar.android.tsx
+++ src/Toolbar.android.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Keyboard, KeyboardEvent, StyleSheet } from 'react-native';
 import ToolbarItem from './ToolbarItem';
 
 const CONTROLS = [
@@ -12,13 +12,61 @@ const CONTROLS = [
 ];
 
 const Toolbar = () => {
-  return (
-    <ScrollView horizontal keyboardShouldPersistTaps="always">
-      {CONTROLS.map(item => (
-        <ToolbarItem controlName={item} key={item} testID={`${item}Item`} />
-      ))}
-    </ScrollView>
-  );
+  const [{ isKeyboardVisible, keyboardHeight }, toggleKeyboard] = useState({
+    isKeyboardVisible: false,
+    keyboardHeight: 0,
+  });
+  useEffect(() => {
+    const keyboardShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      handleKeyboardShow
+    );
+    const keyboardHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      handleKeyboardHide
+    );
+    return () => {
+      keyboardShowListener.remove();
+      keyboardHideListener.remove();
+    };
+  }, []);
+
+  const handleKeyboardShow = (keyboardEvent: KeyboardEvent) => {
+    if (!keyboardEvent.endCoordinates) {
+      return;
+    }
+    const keyboardHeight = keyboardEvent.endCoordinates.height;
+
+    toggleKeyboard({ isKeyboardVisible: true, keyboardHeight });
+  };
+
+  const handleKeyboardHide = () => {
+    toggleKeyboard({ isKeyboardVisible: false, keyboardHeight: 0 });
+  };
+
+  if (isKeyboardVisible) {
+    return (
+      <ScrollView
+        horizontal
+        keyboardShouldPersistTaps="always"
+        showsHorizontalScrollIndicator={false}
+        style={[styles.container, { bottom: keyboardHeight }]}
+      >
+        {CONTROLS.map(item => (
+          <ToolbarItem controlName={item} key={item} testID={`${item}Item`} />
+        ))}
+      </ScrollView>
+    );
+  }
+  return null;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    left: 0,
+    position: 'absolute',
+    right: 0,
+  },
+});
 
 export default Toolbar;
