2be58c755c46a6b488b1acafaa3a558edcc427b1
Michal Kantor <kantorm96@gmail.com>
2020-02-25 09:29:09 +0100

    fix: show accessory view only for markdown input on Android

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 
 // @ts-ignore
 import { MarkdownInput } from 'react-native-markdown-input';
@@ -8,6 +8,11 @@ export default function App() {
   return (
     <View style={styles.container}>
       <MarkdownInput />
+
+      <TextInput
+        placeholder="Different Input without accessory view"
+        style={styles.inputWithoutAccessory}
+      />
     </View>
   );
 }
@@ -18,5 +23,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     flex: 1,
     justifyContent: 'center',
+  },
+
+  inputWithoutAccessory: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 40,
+    marginTop: 20,
+    paddingHorizontal: 5,
+    width: 300,
   },
 });
