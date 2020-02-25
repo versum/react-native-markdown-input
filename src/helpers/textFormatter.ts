73892dd6f5429f4de7cf088bdee2aee26587a703
Michal Kantor <kantorm96@gmail.com>
2020-02-25 13:00:53 +0100

    feat: very initial implementation

--- src/helpers/textFormatter.ts
+++ src/helpers/textFormatter.ts
@@ -1,3 +1,42 @@
-export default (_controlName: string, inputValue: string | undefined) => {
-  return inputValue;
+enum MarkdownSymbols {
+  bold = '**',
+  italic = '_',
+  link = '[]()',
+  heading = '#',
+}
+export default (controlName: string, inputValue: string = '') => {
+  switch (controlName) {
+    case 'bold': {
+      return addBold(inputValue);
+    }
+    case 'italic': {
+      return addItalic(inputValue);
+    }
+    case 'link': {
+      return addLink(inputValue);
+    }
+    case 'heading': {
+      return addHeading(inputValue);
+    }
+
+    default: {
+      return inputValue;
+    }
+  }
+};
+
+const addBold = (inputValue: string) => {
+  return `${inputValue}${MarkdownSymbols.bold}${MarkdownSymbols.bold}`;
+};
+
+const addItalic = (inputValue: string) => {
+  return `${inputValue}${MarkdownSymbols.italic}${MarkdownSymbols.italic}`;
+};
+
+const addLink = (inputValue: string) => {
+  return `${inputValue}${MarkdownSymbols.link}`;
+};
+
+const addHeading = (inputValue: string) => {
+  return `${MarkdownSymbols.heading}${inputValue}`;
 };
