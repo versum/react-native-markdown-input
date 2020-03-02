5b9f8a1b76ce09530d725fde435ecf49dba5e79e
Michal Kantor <kantorm96@gmail.com>
2020-03-02 13:09:17 +0100

    chore: initial docz setup

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-commonjs
+exports.onCreateWebpackConfig = args => {
+  args.actions.setWebpackConfig({
+    resolve: {
+      alias: {
+        'react-native': 'react-native-web',
+      },
+    },
+  });
+};
