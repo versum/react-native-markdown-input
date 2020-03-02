c4253bb547da1d5d252b3eab0dc5623f3bee6a02
Michal Kantor <kantorm96@gmail.com>
2020-03-02 13:09:17 +0100

    chore: initial docz setup

---
+++ src/components/Toolbar.web.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { ScrollView, Keyboard, KeyboardEvent, StyleSheet } from 'react-native';
+
+import { ToolbarProps } from '../componentTypes';
+
+import ToolbarItem from './ToolbarItem';
+
+// It is a copy of android toolbar created for web documentation
+const Toolbar = ({
+  CustomToolbarItem,
+  controls,
+  isFocused,
+  handleItemPress,
+}: ToolbarProps) => {
+  const [keyboardHeight, setKeyboardHeight] = useState(0);
+
+  useEffect(() => {
+    const keyboardShowListener = Keyboard.addListener(
+      'keyboardDidShow',
+      handleKeyboardShow
+    );
+    const keyboardHideListener = Keyboard.addListener(
+      'keyboardDidHide',
+      handleKeyboardHide
+    );
+    return () => {
+      keyboardShowListener.remove();
+      keyboardHideListener.remove();
+    };
+  }, []);
+
+  const handleKeyboardShow = (keyboardEvent: KeyboardEvent) => {
+    if (!keyboardEvent.endCoordinates) {
+      return;
+    }
+    const keyboardHeight = keyboardEvent.endCoordinates.height;
+
+    setKeyboardHeight(keyboardHeight);
+  };
+
+  const handleKeyboardHide = () => {
+    setKeyboardHeight(0);
+  };
+
+  if (keyboardHeight !== 0 && isFocused) {
+    return (
+      <ScrollView
+        horizontal
+        keyboardShouldPersistTaps="always"
+        showsHorizontalScrollIndicator={false}
+        style={[styles.container, { bottom: keyboardHeight }]}
+      >
+        {controls.map(item =>
+          CustomToolbarItem !== undefined ? (
+            <CustomToolbarItem
+              controlName={item}
+              handleItemPress={handleItemPress}
+              key={item}
+              testID={`${item}Item`}
+            />
+          ) : (
+            <ToolbarItem
+              controlName={item}
+              handleItemPress={handleItemPress}
+              key={item}
+              testID={`${item}Item`}
+            />
+          )
+        )}
+      </ScrollView>
+    );
+  }
+  return null;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#eee',
+    elevation: 1,
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    zIndex: 1,
+  },
+});
+
+export default Toolbar;
